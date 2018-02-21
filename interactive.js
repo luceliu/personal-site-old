b714d7f37acd5b19df3740b052e9de3bcba69403
Luce <lucy_liu@live.ca>
2018-02-20 16:16:17 -0800

    can toggle identities now :-)

--- interactive.js
+++ interactive.js
@@ -1,18 +1,39 @@
-var state = "developer";
-
+var state;
 $(function() {
   console.log("ready!");
-  if (state === "developer") {
-    $("#student").hide();
-    $("#person").hide();
-    $("#developer").show();
-  } else if (state === "student") {
-    $("#developer").hide();
-    $("#person").hide();
-    $("#student").show();
-  } else if (state === "person") {
-    $("#developer").hide();
-    $("#student").hide();
-    $("#person").show();
-  }
+  state = "developer";
+  updateText();
 });
+
+$(".dropdown-item").click(handleClick);
+
+function handleClick() {
+  var id = $(this).attr("id");
+  if (id === "developer-btn") {
+    state = "developer";
+  } else if (id === "student-btn") {
+    state = "student";
+  } else if (id === "person-btn") {
+    state = "person";
+  }
+  updateText();
+}
+
+function updateText() {
+  if (state === "developer") {
+    $("#student").addClass("hidden");
+    $("#person").addClass("hidden");
+    $("#developer").removeClass("hidden");
+    $("#dropdownMenuButton").text("developer");
+  } else if (state === "student") {
+    $("#developer").addClass("hidden");
+    $("#person").addClass("hidden");
+    $("#student").removeClass("hidden");
+    $("#dropdownMenuButton").text("student");
+  } else if (state === "person") {
+    $("#developer").addClass("hidden");
+    $("#student").addClass("hidden");
+    $("#person").removeClass("hidden");
+    $("#dropdownMenuButton").text("person");
+  }
+}
